ff96d4b94eb569f6a5df8b5eab7bb0ac909f3538
emil <Emil.Aminov@tatar.ru>
2021-04-14 01:28:56 +0300

    :sparkles:

--- service_worker/src/worker.py
+++ service_worker/src/worker.py
@@ -92,11 +92,13 @@ def create_app():
 
 app = create_app()
 
+
 def run():
     import threading
     thread1 = threading.Thread(target=signals)
     thread1.start()
-    app.run(host="0.0.0.0", port=8080, debug=True, threaded=True)
+    app.run(host="0.0.0.0", port=8000, debug=True, threaded=True)
+
 
 if __name__ == "__main__":
     run()
