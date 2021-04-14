99597108994d8141ff6b646def1612b53a7d3d55
Vladislav <vladalekseev9@yandex.ru>
2021-04-14 17:29:31 +0300

    :zap:

--- sensors-dashboard/src/index.js
+++ sensors-dashboard/src/index.js
@@ -1,30 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Root as App } from './components/Root';
-import { AppContainer } from 'react-hot-loader'
+import App from './components/Root';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById('root')
 
-render(
-  <AppContainer>
-      <App />
-  </AppContainer>,
-  rootElement
-)
-
-if (module.hot) {
-  module.hot.accept('./components/Root', () => {
-      const NewRoot = require('./components/Root').default
-      render(
-          <AppContainer>
-              <NewRoot />
-          </AppContainer>,
-          rootElement
-      )
-  })
-}
+render(<App />,rootElement)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
